5603bc6f0a6f67450462910cb526df547d1ecd56
Unknown <nxh0809@gmail.com>
2019-01-07 09:23:02 +0700

    Merge remote-tracking branch 'origin/dev' into dev
    Travis test
    
    
    Update environment
    
    
    Update env
    
    
    Test encrypt
    
    
    .
    
    
    .
    
    
    Ssh remote
    
    build ignore
    
    .

--- src/store/context/GlobalContext.tsx
+++ src/store/context/GlobalContext.tsx
@@ -1,6 +1,7 @@
 import {LocationDescriptorObject, History} from 'history';
 import {Breakpoint} from '@material-ui/core/styles/createBreakpoints';
 import {createContext} from 'react';
+import {match} from 'react-router';
 
 export const GlobalContext = createContext<IGlobalContext | any>(null);
 
@@ -8,4 +9,5 @@ export interface IGlobalContext {
   location: LocationDescriptorObject,
   history: History
   width: Breakpoint
+  match: match<any>
 }
